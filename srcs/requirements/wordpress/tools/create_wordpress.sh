073ee03d07fdf70706ac1d8e4f915a0f5ade5e50
o7ab <omaremad951@gmail.com>
2023-04-29 18:31:53 +0400

    pushing new

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,28 +1,10 @@
 #!/bin/sh
 
-# if [ -f ./wp-config.php ]
-# then
-# 	echo "wordpress already downloaded"
-# else
-
-# 	wget http://wordpress.org/latest.tar.gz
-# 	tar xfz latest.tar.gz
-# 	mv wordpress/* .
-# 	rm -rf latest.tar.gz
-# 	rm -rf wordpress
-
-# 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-# 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-# 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-# 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-# 	cp wp-config-sample.php wp-config.php
-
-# fi
-
-# exec "$@"
-
-
-cd /var/www/html/wordpress
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+echo "installing now"
 
 # Download wordpress files
 wp core download  --path="/var/www/html/wordpress" --allow-root
@@ -42,5 +24,9 @@ wp user create --path="/var/www/html/wordpress" --allow-root $WP_DB_USER $WP_DB_
 wp plugin update --allow-root --all
 
 # launch php-fpm
+fi
+
+# exec "$@"
 mkdir -p /run/php/
+
 php-fpm7.3 -F
